133640480730c0deb81c773833a85352edb4f655
nihatran <89045243+nihatran@users.noreply.github.com>
2025-10-26 18:00:38 -0400

    Change about to be responsive to tablet sizes

--- components/about.tsx
+++ components/about.tsx
@@ -1,14 +1,14 @@
 export default function About() {
   return (
-    <div className="flex md:flex-col flex-col-reverse justify-end md:justify-between md:h-svh p-6">
-      <div className="flex flex-col md:items-end text-2xl md:text-4xl font-semibold gap-2 md:gap-5 px-3">
+    <div className="flex xl:flex-col flex-col-reverse justify-end md:justify-between xl:h-svh p-6">
+      <div className="flex flex-col xl:items-end text-2xl lg:text-4xl xl:text-4xl font-medium xl:font-semibold gap-2 xl:gap-5 px-3 md:px-0">
         <p>Take on challenges without fear of failure.</p>
         <p>Live each day to the fullest.</p>
         <p>Keep learning.</p>
       </div>
-      <div className="flex flex-col gap-9 md:gap-14 md:items-start mb-6 px-3 md:px-0">
-        <h2 className="md:text-8xl text-5xl font-semibold">ABOUT</h2>
-        <div className="md:w-[40vw] md:leading-19 md:text-6xl text-3xl font-medium md:mb-0 mb-7">
+      <div className="flex flex-col gap-9 xl:gap-14 md:items-start mb-5 px-3 md:px-0">
+        <h2 className="xl:text-8xl text-5xl font-semibold">ABOUT</h2>
+        <div className="xl:w-[40vw] w-full xl:leading-19 text-2xl lg:text-4xl xl:text-6xl font-medium md:mb-0 mb-5">
           <p>Hello! My name is Ni.</p>
           <p>
             I'm a web developer that loves to bring thoughtful designs to life!
