64b270a2194970127dee6982f4d81e021e5f065a
nihatran <89045243+nihatran@users.noreply.github.com>
2025-10-26 22:57:58 -0400

    Add gsap animations to about

--- components/about.tsx
+++ components/about.tsx
@@ -1,20 +1,107 @@
+"use client";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollTrigger);
+
 export default function About() {
+  const aboutSelfRef = useRef<HTMLDivElement>(null);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+  const aboutMainRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    if (!aboutSelfRef.current) return;
+    if (!headingRef.current) return;
+    if (!aboutMainRef.current) return;
+
+    const aboutSelf = aboutSelfRef.current.querySelectorAll("p");
+    const master = gsap.timeline({
+      delay: 0.3,
+      scrollTrigger: { trigger: aboutSelf, start: "top 90%" },
+    });
+
+    aboutSelf.forEach((line, i) => {
+      const splitAboutSelf = SplitText.create(line, {
+        type: "lines",
+        mask: "lines",
+      });
+
+      gsap.set(splitAboutSelf.lines, { yPercent: 100 });
+
+      master.to(
+        splitAboutSelf.lines,
+        {
+          yPercent: 0,
+          duration: 0.9,
+          ease: "expo",
+          stagger: 0.2,
+        },
+        i * 0.3
+      );
+    });
+
+    const splitHeading = SplitText.create(headingRef.current, {
+      type: "chars",
+    });
+
+    gsap.set(splitHeading.chars, { opacity: 0 });
+
+    gsap.to(splitHeading.chars, {
+      scrollTrigger: headingRef.current,
+      stagger: {
+        each: 0.08,
+        from: "start",
+      },
+      duration: 0.9,
+      ease: "expo.out",
+      delay: 0.2,
+      opacity: 1,
+    });
+
+    const splitAboutMain = SplitText.create(aboutMainRef.current, {
+      type: "lines",
+      mask: "lines",
+    });
+
+    gsap.set(splitAboutMain.lines, { yPercent: 100 });
+
+    gsap.to(splitAboutMain.lines, {
+      scrollTrigger: aboutMainRef.current,
+      yPercent: 0,
+      duration: 0.9,
+      ease: "expo",
+      stagger: 0.2,
+    });
+  });
+
   return (
     <div className="flex xl:flex-col flex-col-reverse justify-end md:justify-between xl:h-svh p-6">
-      <div className="flex flex-col xl:items-end text-2xl lg:text-4xl xl:text-4xl font-medium xl:font-semibold gap-2 xl:gap-5 px-3 md:px-0">
+      <div
+        ref={aboutSelfRef}
+        className="flex flex-col xl:items-end text-2xl lg:text-4xl xl:text-4xl font-medium xl:font-semibold gap-2 xl:gap-5 px-3 md:px-0"
+      >
         <p>Take on challenges without fear of failure.</p>
         <p>Live each day to the fullest.</p>
         <p>Keep learning.</p>
       </div>
       <div className="flex flex-col gap-9 xl:gap-14 md:items-start mb-5 px-3 md:px-0">
-        <h2 className="xl:text-8xl text-5xl font-semibold">ABOUT</h2>
-        <div className="xl:w-[40vw] w-full xl:leading-19 text-2xl lg:text-4xl xl:text-6xl font-medium md:mb-0 mb-5">
+        <h2 ref={headingRef} className="xl:text-8xl text-5xl font-semibold">
+          ABOUT
+        </h2>
+        <div
+          ref={aboutMainRef}
+          className="xl:w-[40vw] w-full xl:leading-19 text-2xl lg:text-4xl xl:text-6xl font-medium md:mb-0 mb-5"
+        >
           <p>Hello! My name is Ni.</p>
           <p>
             I'm a web developer that loves to bring thoughtful designs to life!
-            With a<span className="text-slate-400"> mission to amaze</span>, I'm
-            focused on building intuitive and engaging digital experiences for
-            all.
+            With a mission to amaze, I'm focused on building intuitive and
+            engaging digital experiences for all.
           </p>
         </div>
       </div>
